399e17375699127f42cff2b13f8b0e8d68b71a74
evgen melnychenko <jemcua@gmail.com>
2015-10-13 21:35:44 +0300

    dev#exceptions
    new exceptions variables

--- ORM/Exceptions.php
+++ ORM/Exceptions.php
@@ -16,19 +16,78 @@ class Exceptions extends \Berie\ORM
 
 	public function DBConnectionFailure(\PDOException $e)
 	{
-		echo "Error!: " . $e->getMessage() . "<br/>";
-		die();
+		$this->getException($e->getMessage());
+
+		return;
 	}
 
-	public function BuilderDoubleOperationError()
+	public function B0020()
 	{
-		echo "Error!: Can`t call double MySQL operations<br/>";
-		die();
+		$message = '[B0020] ORM: Can`t call double MySQL operations.';
+
+		$this->getException($message);
+
+		return;
 	}
 
-	public function BuilderDoubleTableError()
+	public function B0025()
 	{
-		echo "Error!: Can`t call more than one MySQL tables for operation<br/>";
-		die();
+		$message = '[B0025] ORM: Can`t call more than one MySQL tables for operation.';
+
+		$this->getException($message);
+
+		return;
+	}
+
+	public function B0040()
+	{
+		$message = '[B0040] ORM: Can`t execute SET in SELECT and DELETE.';
+
+		$this->getException($message);
+
+		return;
+	}
+
+	public function B0050()
+	{
+		$message = '[B0050] ORM: Can`t execute WHERE in INSERT.';
+
+		$this->getException($message);
+
+		return;
+	}
+
+	public function B0060()
+	{
+		$message = '[B0060] ORM: Can`t execute ORDER BY in INSERT and UPDATE or call this function twice';
+
+		$this->getException($message);
+
+		return;
+	}
+
+	public function B0065()
+	{
+		$message = '[B0065] ORM: Can`t execute LIMIT in INSERT and UPDATE or call this function twice';
+
+		$this->getException($message);
+
+		return;
+	}
+
+	public function B0070()
+	{
+		$message = '[B0070] ORM: Can`t execute OFFSET in INSERT and UPDATE or call this function twice';
+
+		$this->getException($message);
+
+		return;
+	}
+
+	private function getException($message)
+	{
+		trigger_error($message);
+		error_log($message);
+		die;
 	}
 }
